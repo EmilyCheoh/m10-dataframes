c9b215f0ccd2ab1f7e36d3a9ea0cf7acd1276d1e
Emily Qiao <Emily@Emilys-Mac.local>
2017-04-12 11:48:48 -0700

    class exercise

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,37 +2,47 @@
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
 
+data(USPersonalExpenditure)
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
 
+is.data.frame((USPersonalExpenditure))
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
 
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
 
+USPersonalExpenditure.data.frame <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
 
+col.name <- colnames(USPersonalExpenditure.data.frame)
 
 # Why are they so strange?
 
+# all start with X becasue it doesn't allow coloum's name start with numbers 
 
 # What are the row names of your dataframe?
 
+row.name <- rownames(USPersonalExpenditure.data.frame)
 
 # Create a column `category` that is equal to your rownames
 
+USPersonalExpenditure.data.frame$category <- rownames(USPersonalExpenditure.data.frame)
 
 # How much money was spent on personal care in 1940?
 
+pc.1940 <- USPersonalExpenditure.data.frame["Personal Care", "X1940"] # row first, then coloum
 
 # How much money was spent on Food and Tobacco in 1960
 
+ft.1960 <- USPersonalExpenditure.data.frame["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
 
+highest.1960 <- USPersonalExpenditure.data.frame[]
 
 ### Bonus ###
 
--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,34 +1,46 @@
 # Exercise 5: Gates Foundation Educational Grants
 
+# get working directory
+setwd("~/Desktop/INFO201/m10-dataframes/exercise-5")
+
 # Read data into a variable called `grants` using the `read.csv` function
 
+grants <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at your data
 
+View(grants)
 
 # Create a variable `spending` as the `total_amount` column of the dataset
 
+spending <- grants$total_amount #best way to get access to a column is to use the doller sign
 
 # Confirm that your `spending` variable is a vector using the `is.vector` function
 
+is.vector(spending)
 
 # Create a variable `org` as the `organization` column of the dataset.
 # Unfortunately, it will not be a vector by default, so you must transform it using the as.vector function
 
+org <- grants$organization
 
 ### Now you can ask some more interesting questions about the dataset.  Store your answers in variables ###
 
 # What was the mean grant value?
 
+mean.grant <- mean(spending)
 
 # What was the dollar amount of the largest grant?
 
+largest.grant <- max(spending)
 
 # What was the dollar amount of the smallest grant?
 
+smallest.grant <- min(spending)
 
 # Which organization received the largest grant?
 
+largest.recipient <- org[spending == largest.grant]
 
 # Which organization received the smallest grant?
 
